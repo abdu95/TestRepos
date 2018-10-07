f53e730fef8f84b08a41a0495a7372fdb7166ff1
wertu010101 <malikov_abduvosid@mail.rugit config --global user.email malikov_abduvosid@mail.ru>
2018-10-08 03:26:32 +0500

    First commit

---
+++ FizzBuzz/FizzBuzz.Library.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzz.Library.Tests
+{
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            int input = 1;
+            string output = FizzBuzzer.GetValue(input);
+        }
+    }
+}
